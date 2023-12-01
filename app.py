152c708b3a59b3b08092f5951d3e23803e8757a3
Felipe Oliveira <149281400+FellpsR@users.noreply.github.com>
2023-12-01 16:16:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify
+from datetime import datetime, timedelta
+import psycopg2
+
+app = Flask(__name__)
+
+# Configurações do banco de dados PostgreSQL
+db_config = {
+    'host': '192.168.25.37',
+    'database': 'asgard',
+    'user': 'homologacaoUser',
+    'password': 'Cartorio-2024',
+    'port': '5432',
+}
+
+
+
+# Rota para consultar um protocolo por número
+@app.route('/consulta_protocolo/')
+def consulta_protocolo():
+    try:
+
+        # Conecta ao banco de dados PostgreSQL
+        conn = psycopg2.connect(**db_config)
+
+        # Cria um cursor para executar consultas
+        cursor = conn.cursor()
+
+        # Exemplo de consulta filtrando pelo número do protocolo
+        cursor.execute("SELECT * FROM protocolo WHERE protocolo_externo is not null and dominio='PROTOCOLO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
+
+        # Obtém os resultados da consulta
+        resultado = cursor.fetchall()
+
+        # Fecha o cursor e a conexão
+        cursor.close()
+        conn.close()
+
+        # Se o protocolo existir, converte para um formato JSON e o retorna
+        if resultado:
+            return jsonify(resultado)
+        else:
+            return jsonify({'mensagem': 'Protocolo não encontrado'}), 404
+
+    except Exception as e:
+        return f"Erro na consulta: {str(e)}"
+    
+
+@app.route('/consulta_certidao/')
+def consulta_certidao():
+    try:
+        # Conecta ao banco de dados PostgreSQL
+        conn = psycopg2.connect(**db_config)
+
+        # Cria um cursor para executar consultas
+        cursor = conn.cursor()
+
+        # Exemplo de consulta filtrando pelo número do protocolo
+        cursor.execute("SELECT * FROM protocolo WHERE protocolo_externo is not null and dominio='CERTIDAO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
+
+        # Obtém os resultados da consulta
+        resultado = cursor.fetchall()
+
+        # Fecha o cursor e a conexão
+        cursor.close()
+        conn.close()
+
+        # Se o protocolo existir, converte para um formato JSON e o retorna
+        if resultado:
+            return jsonify(resultado)
+        else:
+            return jsonify({'mensagem': 'Protocolo não encontrado'}), 404
+
+    except Exception as e:
+        return f"Erro na consulta: {str(e)}"    
+
+if __name__ == '__main__':
+    app.run(debug=True)
