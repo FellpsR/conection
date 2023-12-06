e24c4cc5355c306a386b58cd4e3709eb51c6868c
FellpsR <feliperodriguesrdo@gmail.com>
2023-12-06 17:22:26 -0300

    Filter Intimações

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def consulta_protocolo():
         cursor = conn.cursor()
 
         # Exemplo de consulta filtrando pelo número do protocolo
-        cursor.execute("SELECT * FROM protocolo WHERE protocolo_externo is not null and dominio='PROTOCOLO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
+        cursor.execute("SELECT codigo, dominio, cadastro, tipo_protocolo FROM protocolo WHERE protocolo_externo is not null and dominio='PROTOCOLO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-29'")
 
         # Obtém os resultados da consulta
         resultado = cursor.fetchall()
@@ -55,8 +55,36 @@ def consulta_certidao():
         # Cria um cursor para executar consultas
         cursor = conn.cursor()
 
-        # Exemplo de consulta filtrando pelo número do protocolo
-        cursor.execute("SELECT * FROM protocolo WHERE protocolo_externo is not null and dominio='CERTIDAO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
+        # Exemplo de consulta filtrando pelo número da certidão
+        cursor.execute("SELECT codigo, dominio, cadastro, tipo_protocolo FROM protocolo WHERE protocolo_externo is not null and dominio='CERTIDAO_RI' and status='FINALIZADO' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
+
+        # Obtém os resultados da consulta
+        resultado = cursor.fetchall()
+
+        # Fecha o cursor e a conexão
+        cursor.close()
+        conn.close()
+
+        # Se o protocolo existir, converte para um formato JSON e o retorna
+        if resultado:
+            return jsonify(resultado)
+        else:
+            return jsonify({'mensagem': 'Protocolo não encontrado'}), 404
+
+    except Exception as e:
+        return f"Erro na consulta: {str(e)}"
+
+@app.route('/consulta_intimacao/')
+def consulta_intimacao():
+    try:
+        # Conecta ao banco de dados PostgreSQL
+        conn = psycopg2.connect(**db_config)
+
+        # Cria um cursor para executar consultas
+        cursor = conn.cursor()
+
+        # Exemplo de consulta filtrando pelo número do protocolo de intimação
+        cursor.execute("SELECT codigo, dominio, cadastro, tipo_protocolo, numero_controle_externo FROM protocolo WHERE numero_controle_externo is not null and dominio='PROTOCOLO_RI' and cast (cadastro as date) between '2023-11-01' and '2023-11-15'")
 
         # Obtém os resultados da consulta
         resultado = cursor.fetchall()
