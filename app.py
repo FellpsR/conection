43e82e2a3752c997e0aaa49ae449f80c83869813
FellpsR <feliperodriguesrdo@gmail.com>
2023-12-11 17:16:16 -0300

    Tentativa de Paginação, inclusão button return home, página de resultados

--- app.py
+++ app.py
@@ -14,24 +14,31 @@ db_config = {
     'port': '5432',
 }
 
+# Definindo último dia útil (looping até encontrar o último dia útil)
+def ultimoDiaUtil(data):
+    while data.weekday() >= 5: # 5 = sábado, 6 = domingo
+        data -= timedelta(days=1)
+    return data
+
+
 # Rota principal com formulário de input de datas
 @app.route('/consulta', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        consulta_tipo = request.form['consulta_tipo']
         data_inicio = request.form['data_inicio']
         data_fim = request.form['data_fim']
+        consulta_tipo = request.form['consulta_tipo']
 
         # Conectar ao banco de dados PostgreSQL
         conn = psycopg2.connect(**db_config)
         cursor = conn.cursor()
 
         if consulta_tipo == 'protocolo':
-            query = "SELECT codigo, dominio, cadastro, tipo_protocolo FROM protocolo WHERE protocolo_externo IS NOT NULL AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
+            query = "SELECT codigo, dominio, cadastro, tipo_protocolo, protocolo_externo, valor_total, saldo FROM protocolo WHERE protocolo_externo IS NOT NULL AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
         elif consulta_tipo == 'certidao':
-            query = "SELECT codigo, dominio, cadastro, tipo_protocolo FROM protocolo WHERE protocolo_externo IS NOT NULL AND dominio='CERTIDAO_RI' AND status='FINALIZADO' AND cast(cadastro as date) BETWEEN %s AND %s"
+            query = "SELECT codigo, dominio, cadastro, tipo_protocolo, protocolo_externo, valor_total, saldo FROM protocolo WHERE protocolo_externo IS NOT NULL AND dominio='CERTIDAO_RI' AND status='FINALIZADO' AND cast(cadastro as date) BETWEEN %s AND %s"
         elif consulta_tipo == 'intimacao':
-            query = "SELECT codigo, dominio, cadastro, tipo_protocolo, numero_controle_externo FROM protocolo WHERE numero_controle_externo IS NOT NULL AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
+            query = "SELECT codigo, dominio, cadastro, tipo_protocolo, numero_controle_externo, valor_total, saldo FROM protocolo WHERE numero_controle_externo IS NOT NULL AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
         
         cursor.execute(query, (data_inicio, data_fim))
         resultado = cursor.fetchall()
@@ -42,12 +49,12 @@ def index():
         if resultado:
             return render_template('result-list.html', resultado=resultado)
         else:
-            return render-render_template('error.html')
+            return render_template('error.html')
 
-    valor_DateInicial = datetime.now().date() - timedelta(days=1);
-    valor_DateFinal = datetime.now().date();
+    valorDateInicial = ultimoDiaUtil(datetime.now().date() - timedelta(days=1));
+    valorDateFinal = datetime.now().date();
 
-    return render_template('index.html', vdi=valor_DateInicial, vdf=valor_DateFinal)
+    return render_template('index.html', vdi=valorDateInicial, vdf=valorDateFinal)
 
 if __name__ == '__main__':
     app.run(debug=True)
