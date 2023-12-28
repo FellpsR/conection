f4ec047ed5f23c58297b1dc8b01c6f78496816e5
FellpsR <feliperodriguesrdo@gmail.com>
2023-12-28 17:00:51 -0300

    Update problematic

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_ONR")
 db = SQLAlchemy(app)
 
@@ -23,22 +24,22 @@ db_config = {
     'port': config("PORT1"),
 }
 
-# Definindo último dia útil (looping até encontrar o último dia útil)
+# Função para encontrar o último dia útil
 def ultimoDiaUtil(data):
-    while data.weekday() >= 5: # 5 = sábado, 6 = domingo
+    while data.weekday() >= 5:  # 5 = sábado, 6 = domingo
         data -= timedelta(days=1)
     return data
-
 
 # Rota principal com formulário de input de datas
 @app.route("/consulta", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # Obter dados do formulário
         data_inicio = request.form["data_inicio"]
         data_fim = request.form["data_fim"]
         consulta_tipo = request.form["consulta_tipo"]
-       
-        # Verificação tratativa de datas no input Inicial menor que a data final
+
+        # Verificar se a data inicial é menor que a data final
         if data_inicio > data_fim:
             error_message = "Data inicial não pode ser maior que a data final."
             return render_template("index.html", error_message=error_message)
@@ -47,7 +48,7 @@ def index():
         conn = psycopg2.connect(**db_config)
         cursor = conn.cursor()
 
-        # Consulta ao banco de dados Asgard
+        # Construir a consultaSQL com base no tipo de consulta
         if consulta_tipo == "protocolo":
             query = "SELECT id, codigo, dominio, status, cadastro, protocolo_externo, valor_total, saldo FROM protocolo WHERE protocolo_externo IS NOT NULL AND protocolo_externo <> '' AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
         elif consulta_tipo == 'certidao':
@@ -56,79 +57,82 @@ def index():
             query = "SELECT id, codigo, dominio, status, cadastro, numero_controle_externo, valor_total, saldo FROM protocolo WHERE numero_controle_externo IS NOT NULL AND numero_controle_externo <> '' AND dominio='PROTOCOLO_RI' AND cast(cadastro as date) BETWEEN %s AND %s"
         elif consulta_tipo == 'pesquisa_qualificada':
             query = "SELECT id, codigo, dominio, status, cadastro, protocolo_externo, valor_total, saldo FROM protocolo WHERE protocolo_externo IS NOT NULL AND protocolo_externo <> '' AND dominio='CERTIDAO_RI' AND status='FINALIZADO' AND cast(cadastro as date) BETWEEN %s AND %s"
-        
+
+        # Executar a consulta SQL
         cursor.execute(query, (data_inicio, data_fim))
         resultado = cursor.fetchall()
 
         cursor.close()
         conn.close()
-        
-        #Função para consultar dados da pasta local
-        def listar_arquivos_com_extensao_recursivamente(diretorio, extensao):
 
+        # Função para listar arquivos com extensão e datas específicas
+        def listar_arquivos_com_extensao_e_datas(diretorio, extensao, data_inicio, data_fim):
             try:
-                # Use a função listdir para obter a lista de arquivos no diretório
                 lista_arquivos = []
-                
+
                 for nome_arquivo in os.listdir(diretorio):
                     caminho_completo = os.path.join(diretorio, nome_arquivo)
+
                     if os.path.isfile(caminho_completo) and nome_arquivo.endswith(extensao):
-                        lista_arquivos.append(caminho_completo)
+
+                        if nome_arquivo.startswith("RelAnalitico_") and nome_arquivo.endswith(".xls"):
+                            partes_nome = nome_arquivo.split('_')
+
+                            if len(partes_nome) >= 3:
+                                data_arquivo_str = partes_nome[2].replace('.xls', '')
+                                data_arquivo = data_arquivo_str
+
+                                if data_inicio <= data_arquivo <= data_fim:
+                                    lista_arquivos.append(caminho_completo)
+
                     elif os.path.isdir(caminho_completo):
-                        lista_arquivos.extend(listar_arquivos_com_extensao_recursivamente(caminho_completo, extensao))
+                        lista_arquivos.extend(listar_arquivos_com_extensao_e_datas(caminho_completo, extensao, data_inicio, data_fim))
+                        print(f'É um diretório: {caminho_completo}')
+
                 return lista_arquivos
-                    
 
             except Exception as e:
                 print(f"Erro ao listar arquivos: {e}")
                 return []
-   
-        diretorio_inicial = config('caminhoDiretorio')
+
+        # Configurações para leitura de arquivos da pasta
+        diretorio_path = config('caminhoDiretorio')
         extensao_desejada = '.xls'
 
-        #Função para ler os arquivos da pasta
-        arquivos = listar_arquivos_com_extensao_recursivamente(diretorio_inicial, extensao_desejada)
+        # Chamar a função para obter a lista de arquivos
+        arquivos = listar_arquivos_com_extensao_e_datas(diretorio_path, extensao_desejada, data_inicio, data_fim)
 
         print(arquivos)
         print(f'Tamanho Resultados: {len(arquivos)}')
-             
 
-        #Retornará os resultados da consulta caso         
+        # Retornar os resultados da consulta
         if resultado:
-            
-            #Detalhes de conexão PostgreSQL
+            # Conectar ao banco de dados
             conn = psycopg2.connect(config("DATABASE_ONR"))
             cursor = conn.cursor()
-            
-            for protocolo in resultado:
 
-                #Verificação se já consta no banco de dados
+            for protocolo in resultado:
+                # Verificar se o protocolo já existe no banco de dados
                 cursor.execute("SELECT id FROM protocolo_asgard WHERE protocolo = %s", (protocolo[1],))
                 existing_record = cursor.fetchone()
 
-                #Se não constar, insere no banco de dados
+                # Se não existir, inserir no banco de dados
                 if not existing_record:
-                                                       
                     query = "INSERT INTO protocolo_asgard (id, protocolo, tipo, status, cadastro, saec, valor_total, saldo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                    values = (protocolo[0], protocolo[1], protocolo[2], protocolo[3], protocolo[4], protocolo [5], protocolo[6], protocolo[7])
-
+                    values = (protocolo[0], protocolo[1], protocolo[2], protocolo[3], protocolo[4], protocolo[5], protocolo[6], protocolo[7])
                     cursor.execute(query, values)
-
-                #Se já constar no banco de dados, atualiza os campos passíveis de atualização
+                # Se existir, atualizar os campos passíveis de atualização
                 else:
                     query = "UPDATE protocolo_asgard SET status = %s, valor_total = %s, saldo = %s WHERE protocolo = %s"
                     values = (protocolo[3], protocolo[6], protocolo[7], protocolo[1])
-                    
                     cursor.execute(query, values)
 
             try:
-                # Certifique-se de commitar para persistir as mudanças
+                # Commitar as mudanças no banco de dados
                 conn.commit()
-
             except Exception as e:
                 print(f"Erro durante a inserção: {e}")
-                #Fazer um rollback das transações
-                conn.rollback()
+                # Fazer rollback em caso de erro
 
             # Feche a conexão
             cursor.close()
